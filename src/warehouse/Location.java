a05f420ad4d9ee25bc62bca099a6f4c5dc9dc95c
ohj595 <ohj595@cs.bham.ac.uk>
2016-02-25 11:42:27 +0000

    Added javadocs in the classes I made/modified

--- src/warehouse/Location.java
+++ src/warehouse/Location.java
@@ -2,31 +2,73 @@ package warehouse;
 
 /**
  * Created by samtebbs on 22/02/2016.
+ * 
+ * Modified by Owen on 25/02/2016:
+ * 
+ * - Added constructor to intialise fields
+ * - Made fields private and added get and set methods
+ * - Added toString method for debugging
+ * - Added javadocs
  */
 public class Location {
 
     private int x, y;
+    
+    /**
+     * Create a location with x and y coordinates.
+     * 
+     * @param x int for x coordinate
+     * @param y int for y coordinate
+     */
+    public Location(int x, int y){
+    	
+    	this.x = x;
+    	this.y = y;
+    }
 
+    /**
+     * To String method for debugging.
+     */
     public String toString(){
     	
     	return "Location (" + this.x + ", " + this.y + ")"; 
     }
     
+    /**
+     * Get method for the x coordinate.
+     * 
+     * @return x coordinate as an int
+     */
     public int getX(){
     	
     	return this.x;
     }
     
+    /**
+     * Get method for the y coordinate.
+     *
+     * @return y coordinate as an int
+     */
     public int getY(){
     	
     	return this.y;
     }
     
+    /**
+     * Set method for the x coordinate.
+     * 
+     * @param x the new integer x coordinate
+     */
     public void setX(int x){
     	
     	this.x = x;
     }
     
+    /**
+     * Set method for the y coordinate.
+     * 
+     * @param y the new integer y coordinate
+     */
     public void setY(int y){
     	
     	this.y = y;
--- src/warehouse/Robot.java
+++ src/warehouse/Robot.java
@@ -2,6 +2,15 @@ package warehouse;
 
 /**
  * Created by samtebbs on 22/02/2016.
+ * 
+ * Modified by Owen on 25/02/2016:
+ * 
+ * - Added boolean 'busy' field for use by job selector
+ * - Added set method for the busy boolean flag
+ * - Added constructor to initialise fields
+ * - Made fields private and added get methods
+ * - Added toString method for debugging
+ * - Added javadocs
  */
 public class Robot {
 
@@ -9,29 +18,62 @@ public class Robot {
     private Location position;
     private boolean busy;
     
+    /**
+     * Create a robot object which contains a name, position and busy boolean flag.
+     * 
+     * @param name the name of this robot
+     */
     public Robot(String name){
     	
     	this.robotName = name;
     	this.busy = false;
     }
     
+    /**
+     * Get method for the robot name.
+     * 
+     * @return the name of this robot
+     */
     public String getName(){
     	
     	return this.robotName;
     }
     
+    /**
+     * Get method for the current position of the robot.
+     * 
+     * @return the location object that describes the current position of the robot
+     */
     public Location getPosition(){
     	
     	return this.position;
     }
     
+    /**
+     * Get method for the busy flag on the robot.
+     * 
+     * @return true if the robot is busy
+     */
     public boolean getBusy(){
     	
     	return this.busy;
     }
     
+    /**
+     * Set method to change the busy flag on the robot.
+     * 
+     * @param bool the new value of busy 
+     */
     public void setBusy(boolean bool){
     	
     	this.busy = bool;
+    }
+    
+    /**
+     * To String method for debugging.
+     */
+    public String toString(){
+    	
+    	return "Robot: " + this.getName() + " - busy:" + this.getBusy();
     }
 }
