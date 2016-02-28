33af79e22e7b7a9455fbe62366c13124a1df706d
Sam Tebbs <samuel.tebbs@yahoo.com>
2016-02-28 23:54:23 +0000

    Added static final instance for global use

--- src/warehouse/util/EventDispatcher.java
+++ src/warehouse/util/EventDispatcher.java
@@ -14,6 +14,7 @@ public class EventDispatcher {
 
     public static final EventDispatcher INSTANCE = new EventDispatcher();
     private HashMap<Object, HashMap<Class, Method>> subscribers = new HashMap<>();
+    public static final EventDispatcher INSTANCE = new EventDispatcher();
 
     /**
      * Static wrapper for INSTANCE.onEvent()
