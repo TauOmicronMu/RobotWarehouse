a099a7fac2ad86d056cac8925c3447b55480c270
axg539 <axg539@bham.ac.uk>
2016-03-02 12:58:19 +0000

    Fixed coordinate system

--- src/warehouse/routePlanning/Map.java
+++ src/warehouse/routePlanning/Map.java
@@ -3,28 +3,28 @@ package warehouse.routePlanning;
 import java.util.HashMap;
 
 import rp.robotics.mapping.GridMap;
-import rp.robotics.testing.TestMaps;
+import rp.robotics.mapping.MapUtils;
 import warehouse.Location;
 
 public class Map {
 	private HashMap<Location, Boolean> available;
 	private Location [][] map;
 	public Map(){
-		GridMap providedMap = TestMaps.warehouseMap();
+		GridMap providedMap = MapUtils.createRealWarehouse();
 		available = new HashMap<Location, Boolean>();
-		map = new Location [providedMap.getYSize() + 2][providedMap.getXSize() + 2];
-		for(int y = 0; y < map.length; y++){
-			for(int x = 0; x < map[y].length; x++){
-				map[y][x] = new Location(x, y);
-				if(providedMap.isObstructed(x -1, y -1)){
-					System.out.print("X");
-					available.put(map[y][x], false);
+		int sizeY = providedMap.getYSize();
+		int sizeX = providedMap.getXSize();
+		map = new Location [sizeY][sizeX];
+		for(int y = 0; y < sizeY; y++){
+			for(int x = 0; x < sizeX; x++){
+				Location temp = new Location(x, (sizeY - 1) - y);
+				map[(sizeY - 1) - y][x] = temp;
+				if(providedMap.isObstructed(x, y)){
+					available.put(temp, false);
 				}else{
-					System.out.print("-");
-					available.put(map[y][x], true);
+					available.put(temp, true);
 				}
 			}
-			System.out.println();
 		}
 	}
 	
---
+++ src/warehouse/routePlanning/TSP.java
@@ -0,0 +1,5 @@
+package warehouse.routePlanning;
+
+public class TSP {
+
+}
