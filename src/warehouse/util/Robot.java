306247d58c17f61570d44f3636cae2914f42b341
SamTebbs33 <Willow33>
2016-03-03 19:45:57 +0000

    Added rotation to Robot

--- src/warehouse/util/Robot.java
+++ src/warehouse/util/Robot.java
@@ -4,6 +4,7 @@ public class Robot {
 
     public String robotName;
     public Location position;
+    public double rotation;
 
     public Robot(String robotName, Location position) {
         this.robotName = robotName;
