15b65b1ec69cd9860adb02ff82f540a2153b57aa
axg539 <axg539@bham.ac.uk>
2016-03-03 09:28:45 +0000

    TSP not working yet

--- src/warehouse/routePlanning/Test.java
+++ src/warehouse/routePlanning/Test.java
@@ -8,6 +8,7 @@ import warehouse.Route;
 public class Test {
 
 	public static void main(String[] args) {
+		System.out.println("DFDSHFBHFJHKGFJDSF");
 		Search s = new Search();
 		Optional<Route> o = s.getRoute(new Location(0,0), new Location(4, 7));
 		if (o.isPresent()) {
