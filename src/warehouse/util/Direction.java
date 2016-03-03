235ad1afd9a805e1dca34fb7e8e19b70754aedd3
SamTebbs33 <samuel.tebbs@yahoo.com>
2016-03-03 22:28:24 +0000

    Added Direction enum

---
+++ src/warehouse/util/Direction.java
@@ -0,0 +1,22 @@
+package warehouse.util;
+
+/**
+ * Created by samtebbs on 03/03/2016.
+ */
+public enum Direction {
+
+    NORTH, EAST, SOUTH, WEST;
+
+    public Direction turnRight() {
+        int i = ordinal() + 1;
+        if (i >= values().length) i = 0;
+        return values()[i];
+    }
+
+    public Direction turnLeft() {
+        int i = ordinal();
+        if (i <= 0) i = values().length - 1;
+        return values()[i];
+    }
+
+}
