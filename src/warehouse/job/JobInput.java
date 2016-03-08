298320745681719dffe31eef69df3bdd74205550
Sam Tebbs <samuel.tebbs@yahoo.com>
2016-03-08 01:42:09 +0000

    Removed redundant code and added comments to JobInput

--- src/warehouse/job/JobInput.java
+++ src/warehouse/job/JobInput.java
@@ -1,36 +1,31 @@
 package warehouse.job;
 
-import java.util.LinkedList;
-import java.util.List;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Scanner;
+import java.util.*;
 
 import warehouse.util.ItemPickup;
 
 public class JobInput {
 	@SuppressWarnings("unused")
-	public static void main(String[] args) throws IOException 
-    {
-        
-        try (BufferedReader reader = new BufferedReader(new FileReader("jobs.csv"))){
-        	Scanner scanner = new Scanner(new File("filename.csv"));
-        	String id;
+	public static void main(String[] args) throws IOException {
+		// Parse locations
+        	Scanner scanner = new Scanner(new File("locations.csv"));
+        	HashMap<String, Location> itemLocations = new HashMap<>();
         	while(scanner.hasNextLine()) {
-        	    String line = scanner.nextLine();
-                scanner.useDelimiter(",");
-                List<ItemPickup> itemPickupList = new LinkedList<ItemPickup>();
-       			if (scanner.hasNext()) {
-       				id = scanner.next();
-       			}
-        		while (scanner.hasNext()) 
-                {
-        			itemPickupList.add(new ItemPickup(scanner.next(), null, Integer.parseInt(scanner.next())));
-                }
-                scanner.close();
+        		String line = scanner.nextLine();
+       			String[] values = line.split(",");
+       			// Add the item location to the hash map using the values array
+       			// values[0] = item name, values[1] = x coordinate, values[2] = y coordinate
         	}
+        	
+        	// Parse items file and intitialise each item pickup with the locations from itemLocations
+        	// ...
+        	
+        	// Parse jobs file and get ach pickup from the hash map created when parsing the items file
+        	// ...
         }
     }
 }
