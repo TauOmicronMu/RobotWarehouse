8627d03d570e6fdee10bf92ce07061aa8b699844
Owen <owenharrijenkins@aol.co.uk>
2016-03-16 19:41:22 +0000

    Restructured build path

---
+++ src/warehouse/jobselection/cancellation/enums/ItemName.java
@@ -0,0 +1,36 @@
+package warehouse.jobselection.cancellation.enums;
+
+public enum ItemName{
+
+	aa,
+	ab,
+	ac,
+	ad,
+	ae,
+	af,
+	ag,
+	ah,
+	ai,
+	aj,
+	ba,
+	bb,
+	bc,
+	bd,
+	be,
+	bf,
+	bg,
+	bh,
+	bi,
+	bj,
+	ca,
+	cb,
+	cc,
+	cd,
+	ce,
+	cf,
+	cg,
+	ch,
+	ci,
+	cj
+
+}
