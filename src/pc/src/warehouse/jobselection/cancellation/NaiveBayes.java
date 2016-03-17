18fb7a43c23149e2d75c2f02d6bf6d346496a4b8
Owen <owenharrijenkins@aol.co.uk>
2016-03-17 14:11:23 +0000

    Made some progress on Naive Bayes machine learning

--- src/pc/src/warehouse/jobselection/cancellation/NaiveBayes.java
+++ src/pc/src/warehouse/jobselection/cancellation/NaiveBayes.java
@@ -1,9 +1,9 @@
 package warehouse.jobselection.cancellation;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 
 import warehouse.job.Job;
-import warehouse.jobselection.JobWorth;
 
 /**
  * JOB SELECTION - (NaiveBayes class)
@@ -19,21 +19,79 @@ public class NaiveBayes implements CancellationMachine {
 
 	private LinkedList<Job> trainingSet;
 
-	private Distribution pGivenDropLocation;
+	private Distribution DropLocationGivenCancelled = new Distribution(null);
+	private Distribution NumberPickupsGivenCancelled = new Distribution(null);
+	private Distribution NumberDifferentItemsGivenCancelled = new Distribution(null);
+	private Distribution TotalRewardGivenCancelled = new Distribution(null);
+	private Distribution TotalWeightGivenCancelled = new Distribution(null);
+	
+	private Distribution DropLocationGivenNotCancelled = new Distribution(null);
+	private Distribution NumberPickupsGivenNotCancelled = new Distribution(null);
+	private Distribution NumberDifferentItemsGivenNotCancelled = new Distribution(null);
+	private Distribution TotalRewardGivenNotCancelled = new Distribution(null);
+	private Distribution TotalWeightGivenNotCancelled = new Distribution(null);
+	
+	private Descriptor DropLocation = new Descriptor("Drop Location", DropLocationGivenCancelled, DropLocationGivenNotCancelled);
+	private Descriptor NumberPickups = new Descriptor("Number of Pickups", NumberPickupsGivenCancelled, NumberPickupsGivenNotCancelled);
+	private Descriptor NumberDifferentItems = new Descriptor("Number of different Items", NumberDifferentItemsGivenCancelled, NumberDifferentItemsGivenNotCancelled);
+	private Descriptor TotalReward = new Descriptor("Total Reward", TotalRewardGivenCancelled, TotalRewardGivenNotCancelled);
+	private Descriptor TotalWeight = new Descriptor("Total Weight", TotalWeightGivenCancelled, TotalWeightGivenNotCancelled);
 	
 	/**
-	 * Construct a NaiveBayes object with a training set and set up class descriptors
+	 * Construct a NaiveBayes object with a training set and set up class
+	 * descriptors
 	 * 
-	 * @param trainingSet the set of jobs to learn from
+	 * @param trainingSet
+	 *            the set of jobs to learn from
 	 */
 	public NaiveBayes(LinkedList<Job> trainingSet) {
 
 		this.trainingSet = trainingSet;
-
-		for(Job job : trainingSet){
+		
+		//Drop Location:
+		
+		double pnumeratorDropLocation = 0.001;
+		double qnumeratorDropLocation = 0.001;
+		double pdenominatorDropLocation = 0;
+		double qdenominatorDropLocation = 0;
+		double pDropLocation;
+		double qDropLocation;
+		
+		for (Job job : trainingSet){
 			
-			
+			if((this.DropLocation.p.probabilities == null) || this.DropLocation.p.contains(job.dropLocation)){
+				
+				this.DropLocation.p.add(new DPair(job.dropLocation, -1));
+				
+				if(job.cancelledInTrainingSet){
+					
+					qdenominatorDropLocation++;
+				}
+				else{
+					
+					pdenominatorDropLocation++;
+				}
+			}		
 		}
+
+		for(int i = 0; i < this.DropLocation.p.probabilities.size(); i++){
+			
+			pDropLocation = pnumeratorDropLocation/pdenominatorDropLocation;
+			qDropLocation = qnumeratorDropLocation/qdenominatorDropLocation;
+			
+			this.DropLocation.p.probabilities.get(i).probability = pDropLocation;
+			this.DropLocation.q.probabilities.get(i).probability = qDropLocation;
+		}
+		
+		//Number of Pickups:
+			
+		//Number of Different Items:
+			
+		//Total Reward:
+			
+		//Total Weight:
+			
+		
 	}
 
 	/**
@@ -41,35 +99,165 @@ public class NaiveBayes implements CancellationMachine {
 	 */
 	@Override
 	public double getProbability(Job job) {
-		
-		/*TODO
-		 * CANCELLEDPROBABILITY = p(JOBCANCELLED) * ([sigmaproduct from 0 to NUMBER OF DESCRIPTORS] of p(DESCRIPTOR|JOBCANCELLED))
+
+		/*
+		 * TODO CANCELLEDPROBABILITY = p(JOBCANCELLED) * ([sigmaproduct from 0 to NUMBER OF DESCRIPTORS] of p(DESCRIPTOR|JOBCANCELLED))
 		 * 
 		 */
 		return 0;
 	}
+
+	//DESCRIBES AN ARRAY OF DPAIRS
 	
-	private class Distribution{
+	private class Distribution {
+
+		private ArrayList<DPair> probabilities;
+
+		private Distribution(ArrayList<DPair> set) {
+
+			this.probabilities = set;
+		}
+
+		public ArrayList<DPair> probabilities() {
+
+			return this.probabilities;
+		}
 		
-		private double p;
-		private double q;
-		
-		private Distribution(double p, double q){
+		public void add(DPair pair){
 			
+			this.probabilities.add(pair);
+		}
+		
+		public boolean contains(Object descriptor){
+			
+			for(int i = 0; i < this.probabilities.size(); i++){
+				
+				if(this.probabilities.get(i).descriptor.equals(descriptor)){
+					
+					return true;
+				}
+			}
+			
+			return false;
+		}
+		
+		public int getIndexOf(Object descriptor){
+			
+			for(int i = 0; i < this.probabilities.size(); i++){
+				
+				if(this.probabilities.get(i).descriptor.equals(descriptor)){
+					
+					return i;
+				}
+			}
+			
+			return -1;
+		}
+		
+		@Override
+		public boolean equals(Object o){
+			
+			if((o instanceof Distribution) && (((Distribution)o).probabilities.equals(this.probabilities))){
+				
+				return true;
+			}
+			
+			return false;
+		}
+		
+		@Override
+		public int hashCode(){
+			
+			return 0;
+		}
+	}
+
+	//DESCRIBES SOMETHING WE CAN CATEGORISE A JOB BY
+	
+	private class Descriptor {
+
+		private String name;
+		private Distribution p;
+		private Distribution q;
+
+		private Descriptor(String name, Distribution p, Distribution q) {
+
+			this.name = name;
 			this.p = p;
 			this.q = q;
 		}
-		
-		public double p(){
-			
+
+		public String getName() {
+
+			return this.name;
+		}
+
+		public Distribution p() {
+
 			return this.p;
 		}
-		
-		public double q(){
-			
+
+		public Distribution q() {
+
 			return this.q;
 		}
 		
+		@Override
+		public boolean equals(Object o){
+			
+			if((o instanceof Descriptor) && (((Descriptor)o).p.equals(this.p)) && (((Descriptor)o).q.equals(this.q))){
+				
+				return true;
+			}
+			
+			return false;
+		}
+		
+		@Override
+		public int hashCode(){
+			
+			return 0;
+		}
+	}
+
+	//DESCRIBES A LINKED OBJECT AND PROBABILITY
+	
+	private class DPair {
+
+		private Object descriptor;
+		private double probability;
+
+		private DPair(Object a, double b) {
+
+			this.descriptor = a;
+			this.probability = b;
+		}
+
+		public Object descriptor() {
+
+			return this.descriptor;
+		}
+
+		public double probability() {
+
+			return this.probability;
+		}
+		
+		@Override
+		public boolean equals(Object o){
+			
+			if((o instanceof DPair) && ((DPair)o).descriptor.equals(this.descriptor)){
+				
+				return true;
+			}
+			
+			return false;
+		}
+		
+		@Override
+		public int hashCode(){
+			
+			return 0;
+		}
 	}
 }
-
--- src/pc/src/warehouse/jobselection/cancellation/enums/DropLocation.java
+++
@@ -1,6 +0,0 @@
-package warehouse.jobselection.cancellation.enums;
-
-public enum DropLocation {
-
-	drop47, drop77
-}
