c1c98c3e1102117ef710c9a5df1eb70b5e21738d
Sam Tebbs <samuel.tebbs@yahoo.com>
2016-03-21 11:09:56 +0000

    Added BeginAssigningEvent.dropLocaitons

--- shared/src/warehouse/event/BeginAssigningEvent.java
+++ shared/src/warehouse/event/BeginAssigningEvent.java
@@ -1,6 +1,7 @@
 package warehouse.event;
 
 import warehouse.job.Job;
+import warehouse.util.*;
 
 import java.util.*;
 
@@ -13,11 +14,13 @@ import java.util.*;
 public class BeginAssigningEvent {
 	
 	public List<Job> jobs;
+	public List<Location> dropLocations;
 
 	/**
 	 * Create a new event
 	 */
-	public BeginAssigningEvent(List<Job> jobs){
+	public BeginAssigningEvent(List<Job> jobs, List<Location> dropLocations){
 		this.jobs = jobs;
+		this.dropLocations = dropLocations;
 	}
 }
