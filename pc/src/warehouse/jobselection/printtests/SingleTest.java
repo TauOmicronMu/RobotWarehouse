28ee324c999d0ba76e823b5a5927826720173693
axg539 <axg539@bham.ac.uk>
2016-03-22 22:32:18 +0000

    fixed test for final system

--- pc/src/warehouse/jobselection/printtests/SingleTest.java
+++ pc/src/warehouse/jobselection/printtests/SingleTest.java
@@ -73,17 +73,17 @@ public class SingleTest  extends Thread{
 //        files5[4] = filePath + "\\5\\drops.csv";
 //        fileSet.add(files5);
 
-        String[] actualFiles = new String[5];
+        String[] actualFiles = new String[4];
         actualFiles[0] = filePath + "\\actual\\locations.csv";
         actualFiles[1] = filePath + "\\actual\\items.csv";
         actualFiles[2] = filePath + "\\actual\\jobs.csv";
-        actualFiles[3] = filePath + "\\actual\\cancellations.csv";
-        actualFiles[4] = filePath + "\\actual\\drops.csv";
+//        actualFiles[3] = filePath + "\\actual\\cancellations.csv";
+        actualFiles[3] = filePath + "\\actual\\drops.csv";
         fileSet.add(actualFiles);
 
         for(String[] fileNameArray : fileSet) {
 
-            assert (fileNameArray.length == 5);
+            assert (fileNameArray.length == 4);
 
             // Parse locations
             HashMap<String, Location> itemLocations = new HashMap<>();
@@ -105,14 +105,14 @@ public class SingleTest  extends Thread{
                 jobs.put(values[0], new Job(null, jobPickups, values[0]));
             });
             // Parse cancellations file (I'm not sure of the actual file name)
-            assert(jobs != null);
-            
-            parseFile(fileNameArray[3], values -> {
-            	assert(jobs.containsKey(values[0]));
-            	jobs.get(values[0]).cancelledInTrainingSet = values[1].equals("0") ? false : true;
-            });
+//            assert(jobs != null);
+//            
+//            parseFile(fileNameArray[3], values -> {
+//            	assert(jobs.containsKey(values[0]));
+//            	jobs.get(values[0]).cancelledInTrainingSet = values[1].equals("0") ? false : true;
+//            });
             List<Location> dropLocations = new ArrayList<>();
-            parseFile(fileNameArray[4], values -> jobs.values().forEach(job -> job.dropLocation = new Location(Integer.parseInt(values[0]), Integer.parseInt(values[1]))));
+            parseFile(fileNameArray[3], values -> jobs.values().forEach(job -> job.dropLocation = new Location(Integer.parseInt(values[0].trim()), Integer.parseInt(values[1].trim()))));
 
             // Convert the job map to a list
 
