43bbc1069d9c693fbcc6d018bc52bc346508b5ff
Owen <owenharrijenkins@aol.co.uk>
2016-03-22 00:42:21 +0000

    Put some code in place to allow for multiple robots given a few more methods from Route Planning, made a RobotGotLostEvent

---
+++ pc/src/warehouse/jobselection/test/RobotGotLostEvent.java
@@ -0,0 +1,16 @@
+package warehouse.jobselection.test;
+
+import warehouse.util.Robot;
+
+/**
+ * Created by Owen on 21/03/2016.
+ */
+public class RobotGotLostEvent {
+
+    public Robot robot;
+
+    public RobotGotLostEvent(Robot robot){
+
+        this.robot = robot;
+    }
+}
