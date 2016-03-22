e34a7c02425e31559f9a03b55214bd3073346c3d
SamTebbs33 <samuel.tebbs@yahoo.com>
2016-03-22 23:09:53 +0000

    Made job input work with multiple data sets

--- pc/src/warehouse/job/JobInput.java
+++ pc/src/warehouse/job/JobInput.java
@@ -17,36 +17,67 @@ public class JobInput {
 	public static void launch() throws IOException {
 		// Parse locations
 		HashMap<String, Location> itemLocations = new HashMap<>();
-		parseFile("locations.csv", values -> itemLocations.put(values[2], new Location(Integer.parseInt(values[0]), Integer.parseInt(values[1]))));
+        List<Location> dropLocations = new ArrayList<>();
+        HashMap<String, ItemPickup> itemPickups = new HashMap<>();
+        HashMap<String, Job> jobs = new HashMap<>();
+
+        final int numSets = 5;
+
+        repeat(numSets, i -> {
+            try {
+                parseFile(i + "/locations.csv", values -> itemLocations.put(values[2], new Location(Integer.parseInt(values[0]), Integer.parseInt(values[1]))));
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        });
 
 		// Parse items file
-		HashMap<String, ItemPickup> itemPickups = new HashMap<>();
-		parseFile("items.csv", values -> {
-			itemPickups.put(values[0], new ItemPickup(values[0], itemLocations.get(values[0]), 0, Double.parseDouble(values[1]), Double.parseDouble(values[2])));
-		});
+        repeat(numSets, i -> {
+            try {
+                parseFile(i + "/items.csv", values -> itemPickups.put(values[0], new ItemPickup(values[0], itemLocations.get(values[0]), 0, Double.parseDouble(values[1]), Double.parseDouble(values[2]))));
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        });
 
 		// Parse jobs file
-		HashMap<String, Job> jobs = new HashMap<>();
-		parseFile("jobs.csv", values -> {
-			List<ItemPickup> jobPickups = new LinkedList<>();
-			for(int i = 1; i < values.length; i += 2) {
-				ItemPickup p = (ItemPickup) itemPickups.get(values[i]).clone();
-				p.itemCount = Integer.parseInt(values[i+1]);
-				jobPickups.add(p);
-			}
-			jobs.put(values[0], new Job(null, jobPickups, values[0]));
-		});
+		repeat(numSets, i -> {
+            try {
+                parseFile(i + "/jobs.csv", values -> {
+                    List<ItemPickup> jobPickups = new LinkedList<>();
+                    for (int j = 1; j < values.length; j += 2) {
+                        ItemPickup p = (ItemPickup) itemPickups.get(values[j]).clone();
+                        p.itemCount = Integer.parseInt(values[j + 1]);
+                        jobPickups.add(p);
+                    }
+                    jobs.put(values[0], new Job(null, jobPickups, values[0]));
+                });
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        });
 
 		// Parse cancellations file
-		parseFile("cancellations.csv", values -> {
-			jobs.get(values[0]).cancelledInTrainingSet = values[1].equals("0") ? false : true;
-		});
+		repeat(numSets, i -> {
+            try {
+                parseFile(i + "/cancellations.csv", values -> {
+                    jobs.get(values[0]).cancelledInTrainingSet = values[1].equals("0") ? false : true;
+                });
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        });
 
-        List<Location> dropLocations = new ArrayList<>();
-        parseFile("drops.csv", values -> {
-            if(values.length < 2) return;
-            System.out.println(Arrays.toString(values));
-            dropLocations.add(new Location(Integer.parseInt(values[0]), Integer.parseInt(values[1])));
+        repeat(numSets, i -> {
+            try {
+                parseFile(i + "/drops.csv", values -> {
+                    if (values.length < 2) return;
+                    System.out.println(Arrays.toString(values));
+                    dropLocations.add(new Location(Integer.parseInt(values[0]), Integer.parseInt(values[1])));
+                });
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
         });
 
 		// Convert the job map to a list
@@ -56,9 +87,16 @@ public class JobInput {
         	EventDispatcher.onEvent2(new BeginAssigningEvent(jobList, new LinkedList<>()));
 	}
 
-	public static void parseFile(String filePath, Consumer<String[]> consumer) throws FileNotFoundException {
+    private static void repeat(int times, Consumer<Integer> consumer) {
+        for(int i = 0; i < times; i++) consumer.accept(i);
+    }
+
+    public static void parseFile(String filePath, Consumer<String[]> consumer) throws FileNotFoundException {
 		Scanner in = new Scanner(new File(filePath));
-		while(in.hasNextLine()) consumer.accept(in.nextLine().trim().split(","));
+		while(in.hasNextLine()) {
+			String line = in.nextLine();
+			if(!line.isEmpty()) consumer.accept(line.trim().split(","));
+		}
 	}
 
 }
