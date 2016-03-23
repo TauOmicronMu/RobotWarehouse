c38831639d5da111cb4562e6a5ec6cee90de0c6f
SamTebbs33 <samuel.tebbs@yahoo.com>
2016-03-23 15:05:14 +0000

    Re-added nxt

---
+++ nxt/src/warehouse/localisation/SensorModelModified.java
@@ -0,0 +1,157 @@
+package warehouse.localisation;
+
+import lejos.robotics.RangeReadings;
+import rp.robotics.localisation.GridPositionDistribution;
+import rp.robotics.localisation.SensorModel;
+import rp.robotics.navigation.Heading;
+
+/**
+ * 
+ * @author jokLiu
+ *
+ */
+public class SensorModelModified implements SensorModel {
+
+	private RangeReadings readings;
+	private Distances dist;
+	
+	private double constant = 2;
+
+	public SensorModelModified(Distances dist) {
+		this.dist = dist;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see rp.robotics.localisation.SensorModel#updateAfterSensing(rp.robotics.
+	 * localisation.GridPositionDistribution, rp.robotics.mapping.Heading,
+	 * lejos.robotics.RangeReadings)
+	 */
+	@Override
+	public GridPositionDistribution updateAfterSensing(GridPositionDistribution _from, Heading _heading,
+			RangeReadings _readings) {
+		readings = _readings;
+
+		GridPositionDistribution to = new GridPositionDistribution(_from);
+		
+		// Move the probability in the correct direction for the action
+		if (_heading == Heading.PLUS_X) {
+			headPlusX(_from, to);
+		} else if (_heading == Heading.PLUS_Y) {
+			headPlusY(_from, to);
+
+		} else if (_heading == Heading.MINUS_X) {
+			headMinusX(_from, to);
+		} else if (_heading == Heading.MINUS_Y) {
+			headMinusY(_from, to);
+		}
+
+		return to;
+
+	}
+
+	private void headPlusY(GridPositionDistribution _from, GridPositionDistribution to) {
+		float read = readings.get(0).getRange();
+		if (readings.get(0).getRange() <= 2.5f) {
+
+			for (int y = 0; y < to.getGridHeight(); y++) {
+
+				for (int x = 0; x < to.getGridWidth() ; x++) {
+					if ((!to.isObstructed(x, y))) {
+						if (read > dist.getPlusY(x, y)) {
+							if(read > dist.getPlusY(x, y)*constant)
+							to.setProbability(x, y, 0f);
+						}
+						else 
+						{
+							if(read < dist.getPlusY(x, y)/constant)
+							{
+								to.setProbability(x, y, 0f);
+							}
+						}
+					}
+				}
+
+			}
+		}
+	}
+	
+	private void headPlusX(GridPositionDistribution _from, GridPositionDistribution to) {
+		float read = readings.get(0).getRange();
+		if (readings.get(0).getRange() <= 2.5f) {
+
+			for (int y = 0; y < to.getGridHeight(); y++) {
+
+				for (int x = 0; x < to.getGridWidth(); x++) {
+					if ((!to.isObstructed(x, y))) {
+						if (read > dist.getPlusX(x, y)) {
+							if(read > dist.getPlusX(x, y)*constant)
+							to.setProbability(x, y, 0f);
+						}
+						else 
+						{
+							if(read < dist.getPlusX(x, y)/constant)
+							{
+								to.setProbability(x, y, 0f);
+							}
+						}
+					}
+				}
+
+			}
+		}
+	}
+
+	private void headMinusX(GridPositionDistribution _from, GridPositionDistribution to) {
+		float read = readings.get(0).getRange();
+		if (readings.get(0).getRange() <= 2.5f) {
+
+			for (int y = 0; y < to.getGridHeight(); y++) {
+
+				for (int x = 0; x < to.getGridWidth() ; x++) {
+					if ((!to.isObstructed(x, y))) {
+						if (read > dist.getMinusX(x, y)) {
+							if(read > dist.getMinusX(x, y)*constant)
+							to.setProbability(x, y, 0f);
+						}
+						else 
+						{
+							if(read < dist.getMinusX(x, y)/constant)
+							{
+								to.setProbability(x, y, 0f);
+							}
+						}
+					}
+				}
+
+			}
+	}
+	}
+	
+	private void headMinusY(GridPositionDistribution _from, GridPositionDistribution to) {
+		float read = readings.get(0).getRange();
+		if (readings.get(0).getRange() <= 2.5f) {
+
+			for (int y = 0; y < to.getGridHeight(); y++) {
+
+				for (int x = 0; x < to.getGridWidth() ; x++) {
+					if ((!to.isObstructed(x, y))) {
+						if (read > dist.getMinusY(x, y)) {
+							if(read > dist.getMinusY(x, y)*constant)
+							to.setProbability(x, y, 0f);
+						}
+						else 
+						{
+							if(read < dist.getMinusY(x, y)/constant)
+							{
+								to.setProbability(x, y, 0f);
+							}
+						}
+					}
+				}
+
+			}
+		}
+	}
+}
---
+++ nxt/src/warehouse/robot_interface/PickupJob.java
@@ -0,0 +1,112 @@
+package warehouse.robot_interface;
+
+import lejos.nxt.Button;
+import lejos.nxt.ButtonListener;
+import lejos.nxt.LCD;
+import lejos.nxt.Sound;
+import lejos.util.Delay;
+import rp.util.Rate;
+
+/**
+ * The thread starting when a pick up job is ready, and the robot is in the pick
+ * up location
+ * 
+ * @author txs
+ *
+ */
+public class PickupJob {
+
+	private Communication comm;
+	private Boolean jobRun;
+	private String itemName;
+	private int itemNumber;
+	private boolean enterPressed, escapePressed;
+
+	/**
+	 * Constructor
+	 * 
+	 * @param comm
+	 * @param jobRun
+	 */
+	public PickupJob(Communication comm, Boolean jobRun) {
+		this.comm = comm;
+		this.jobRun = jobRun;
+		itemName = comm.getItemName();
+		itemNumber = comm.getItemNumber();
+		enterPressed = false;
+		escapePressed = false;
+
+		Button.ENTER.addButtonListener(new ButtonListener() {
+
+			@Override
+			public void buttonReleased(Button b) {
+				enterPressed = true;
+			}
+
+			@Override
+			public void buttonPressed(Button b) {
+			    //TODO : ???
+			}
+		});
+		
+		Button.ESCAPE.addButtonListener(new ButtonListener() {
+
+			@Override
+			public void buttonReleased(Button b) {
+				escapePressed = true;
+			}
+
+			@Override
+			public void buttonPressed(Button b) {
+				//TODO : ???
+			}
+		});
+	}
+
+	public void run() {
+		updateScreen();
+
+		while (jobRun && itemNumber > 0) {
+			if (enterPressed) {
+				itemNumber--;
+				enterPressed = false;
+
+				updateScreen();
+				
+				Sound.playTone(600, 30);
+				Delay.msDelay(30);
+			}
+
+			if (escapePressed) {
+				itemNumber++;
+
+				CancelMenu cancelMenu = new CancelMenu(comm, jobRun);
+				cancelMenu.run();
+
+				escapePressed = false;
+			}
+
+			Rate rate = new Rate(20);
+			rate.sleep();
+		}
+
+		if (itemNumber == 0) {
+			Sound.playTone(440, 30);
+			Delay.msDelay(30);
+			comm.jobDone();
+		}
+		else {
+			Sound.playTone(200, 30);
+			Delay.msDelay(30);
+		}
+	}
+
+	/**
+	 * Updating the screen with the remaining number of items
+	 */
+	private void updateScreen() {
+		LCD.clearDisplay();
+		LCD.drawString("Item: " + itemName, 0, 0);
+		LCD.drawString("Remaining: " + itemNumber, 0, 1);
+	}
+}
