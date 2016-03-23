1867056cdc7a2fa918f1c5ae5af232349fc65211
SamTebbs33 <samuel.tebbs@yahoo.com>
2016-03-23 16:48:10 +0000

    Added warehouse main

--- pc/src/warehouse/main/WarehouseMain.java
+++ pc/src/warehouse/main/WarehouseMain.java
@@ -1,14 +1,37 @@
 package warehouse.main;
 
+import warehouse.event.BeginAssigningEvent;
+import warehouse.job.AssignedJob;
+import warehouse.job.JobInput;
+import warehouse.management_interface.Main;
+import warehouse.networking.WarehouseServer;
+import warehouse.util.Robot;
+import warehouse.util.Subscriber;
+
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * Created by sxt567 on 23/03/16.
  */
 public class WarehouseMain {
 
-    public static void main(String[] args) {
+    private static WarehouseServer server;
+
+    public static void main(String[] args) throws IOException {
         // Start server
+        server = new WarehouseServer();
         // Start job input
-        // Start GUI
+        JobInput.launch();
+    }
+
+    @Subscriber
+    public static void onBeginAssigning(BeginAssigningEvent event) {
+        List<String> robotNames = new LinkedList<>();
+        for(Robot robot : server.robots) if(robot != null) robotNames.add(robot.robotName);
+        Main.setParameters(event.jobs, new LinkedList<AssignedJob>(), robotNames);
+        Main.startGUI();
     }
 
 }
