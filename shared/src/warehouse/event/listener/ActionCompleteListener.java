2cf6efa7be7c9da8702720bf652c1f42df5a282f
SamTebbs33 <samuel.tebbs@yahoo.com>
2016-03-23 22:17:41 +0000

    Made all the dank listener

---
+++ shared/src/warehouse/event/listener/ActionCompleteListener.java
@@ -0,0 +1,10 @@
+package warehouse.event.listener;
+
+import warehouse.event.ActionCompleteEvent;
+
+/**
+ * Created by sxt567 on 23/03/16.
+ */
+public interface ActionCompleteListener {
+    public void onActionComplete(ActionCompleteEvent e);
+}
---
+++ shared/src/warehouse/event/listener/DropOffReachedListener.java
@@ -0,0 +1,10 @@
+package warehouse.event.listener;
+
+import warehouse.event.DropOffReachedEvent;
+
+/**
+ * Created by sxt567 on 23/03/16.
+ */
+public interface DropOffReachedListener {
+    public void onDropOffReached(DropOffReachedEvent e);
+}
