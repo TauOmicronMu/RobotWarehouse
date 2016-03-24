84233ddd122383d24553646f5f08ad6297ef260d
SamTebbs33 <samuel.tebbs@yahoo.com>
2016-03-24 00:31:04 +0000

    Moved robot event manager variables to RobotEventManager

--- shared/src/warehouse/event/manager/RobotOffManager.java
+++ shared/src/warehouse/event/manager/RobotOffManager.java
@@ -8,18 +8,6 @@ import warehouse.event.listener.RobotOffListener;
  */
 public class RobotOffManager extends RobotEventManager<RobotOffListener, RobotOffEvent> {
 
-    public static ActionCompleteManager actionCompleteManager = new ActionCompleteManager();
-    public static BeginAssigningManager beginAssigningManager = new BeginAssigningManager();
-    public static DropOffReachedManager dropOffReachedManager = new DropOffReachedManager();
-    public static JobAssignedManager jobAssignedManager = new JobAssignedManager();
-    public static JobCancellationMAnager jobCancellationMAnager = new JobCancellationMAnager();
-    public static JobCompleteManager jobCompleteManager = new JobCompleteManager();
-    public static PickupCompleteManager pickupCompleteManager = new PickupCompleteManager();
-    public static PickupReachedManager pickupReachedManager = new PickupReachedManager();
-    public static RobotLostManager robotLostManager = new RobotLostManager();
-    public static RobotOffManager robotOffManager = new RobotOffManager();
-    public static WrongPlaceManager wrongPlaceManager = new WrongPlaceManager();
-
     @Override
     public void each(RobotOffListener robotOffListener, RobotOffEvent robotOffEvent) {
         robotOffListener.onRobotOff(robotOffEvent);
