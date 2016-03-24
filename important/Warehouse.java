f9880e34f9ec67cde3372b76601c80975d4624f6
TauOmicronMu <TauOmicronMuRS@Gmail.com>
2016-03-24 16:17:48 +0000

    :'(

--- important/Warehouse.java
+++ important/Warehouse.java
@@ -4,15 +4,6 @@ import java.io.*;
 class Warehouse {
 
     public static void main(String[] args) {
-        Format input1 = new AudioFormat(AudioFormat.MPEGLAYER3);
-        Format input2 = new AudioFormat(AudioFormat.MPEG);
-        Format output = new AudioFormat(AudioFormat.LINEAR);
-        PlugInManager.addPlugIn(
-                "com.sun.media.codec.audio.mp3.JavaDecoder",
-                new Format[]{input1, input2},
-                new Format[]{output},
-                PlugInManager.CODEC
-        );
         try{
             Player player = Manager.createPlayer(new MediaLocator(new File("stillalive1.mp3").toURI().toURL()));
             player.start();
