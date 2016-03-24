e3f6922f8cb29da76810fa25a5bb10a49a181c3f
jokLiu <liutkusjokubas@gmail.com>
2016-03-24 10:02:38 +0000

    Some comments added

--- nxt/src/warehouse/localisation/LSlListenerTest.java
+++ nxt/src/warehouse/localisation/LSlListenerTest.java
@@ -1,20 +1,40 @@
 package warehouse.localisation;
-
+	
 import lejos.nxt.LightSensor;
 
+/**
+ * The Class LSlListenerTest to listen for the light sensors to be on the black tape
+ */
 public class LSlListenerTest extends Thread {
 
+	/** The robot unit. */
 	private LineFollowTest robotUnit;
+	
+	/** The right. */
 	private LightSensor right;
+	
+	/** The left. */
 	private LightSensor left;
+	
+	/** The stop. */
 	public static boolean stop = true;
 
+	/**
+	 * Instantiates a new listener 
+	 *
+	 * @param robotUnit the robot unit
+	 * @param right the right
+	 * @param left the left
+	 */
 	public LSlListenerTest(LineFollowTest robotUnit, LightSensor right, LightSensor left) {
 		this.robotUnit = robotUnit;
 		this.right = right;
 		this.left = left;
 	}
 
+	/* (non-Javadoc)
+	 * @see java.lang.Thread#run()
+	 */
 	@Override
 	public void run() {
 		int rightlightValue;
